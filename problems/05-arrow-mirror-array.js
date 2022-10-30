ca9ecbb7ce8f978408307aee00de2f3a43fe7618
Lloyd Stevens <stevens.lloyd@gmail.com>
2022-10-30 08:39:03 +0000

    updated so pushes to new array

--- problems/05-arrow-mirror-array.js
+++ problems/05-arrow-mirror-array.js
@@ -10,7 +10,19 @@ arrowMirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
 arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
 ***********************************************************************/
 
-// your code here
+// arrow function
+const arrowMirrorArray = (array) => {
+  const newArray=array;
+  // loop from the end of the array
+  for (let i=array.length-1; i>=0; i--){
+    // push each element onto the end of the new array to create mirror image of position. 
+    // each pass the end of the array will increase with the new element
+    // as looping downwards it will only mirror image the elements down 
+    // i.e. not repeat elements that have been already pushed. 
+    newArray.push(array[i]);
+  }
+  return newArray;
+};
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
